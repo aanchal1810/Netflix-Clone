a56907fe8b91285b30344d9fdafd2c15de509925
Aanchal <aanchalshah1810@gmail.com>
2025-11-01 19:35:44 +0530

    added edge to edge in java files

--- app/src/main/java/com/example/madminiproject/OnboardingSwipe.java
+++ app/src/main/java/com/example/madminiproject/OnboardingSwipe.java
@@ -8,6 +8,7 @@ import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -39,7 +40,7 @@ public class OnboardingSwipe extends AppCompatActivity implements CardStackListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_swipe);
-
+        EdgeToEdge.enable(this);
         drawerLayout = findViewById(R.id.drawer_layout);
         cardStackView = findViewById(R.id.card_stack_view);
         manager = new CardStackLayoutManager(this, this);
